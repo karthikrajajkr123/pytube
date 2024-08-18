fddf37a3925b881fe7fd78c63211561b38899a45
Karthik Raja <karthikraja90804@gmail.com>
2024-08-18 19:43:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytube',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    python_requires='>=3.6',  # Adjust as needed
+)
